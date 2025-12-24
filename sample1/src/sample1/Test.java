79209a2bd4f6ceef2334d0ce3bdd771f82a79c7a
kimjinseop13579 <kimjinseop090@gmail.com>
2025-12-24 16:57:19 +0900

    Add files via upload

---
+++ sample1/src/sample1/Test.java
@@ -0,0 +1,12 @@
+package sample1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("안녕 자바");
+
+	}
+
+}
---
+++ test/src/com/simple/test/Car.java
@@ -0,0 +1,8 @@
+package com.simple.test;
+
+public class Car {
+    public static void main(String[] args) {
+        System.out.println("안녕 자바");
+
+    }
+}
